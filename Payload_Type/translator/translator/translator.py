9b8ff3d50fe68474594299eb10e4745113de073a
MohamedHdidane <mohamed642hdidane@gmail.com>
2025-09-03 18:59:33 +0100

    help

--- Payload_Type/translator/translator/translator.py
+++ Payload_Type/translator/translator/translator.py
@@ -116,7 +116,7 @@ class MyTranslator(TranslationContainer):
 
         except AttributeError as ae:
             response.Success = False
-            response.Error = f"AttributeError: {str(ae)}. Available attributes: {str(dir(inputMsg))}. Object format: {str(type(inputMsg))} . inputMsg.CryptoKeys: {str(type(inputMsg.CryptoKeys))}"
+            response.Error = f"AttributeError: {str(ae)}. Available attributes: {str(dir(inputMsg))}. Object format: {str(type(inputMsg))} . inputMsg.CryptoKeys: {str(type(inputMsg.CryptoKeys[0]))}"
 
         except Exception as e:
             response.Success = False
